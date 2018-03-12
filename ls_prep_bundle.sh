8fdded3b9c0208545dd58ef54bcc27924da5301c
Casey Deccio <casey@deccio.net>
2018-03-12 15:44:59 -0600

    Add option to rename file

--- ls_prep_bundle.sh
+++ ls_prep_bundle.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-BUNDLE="$1"
-shift
-
-if [ "x$BUNDLE" = "x" ]; then
+usage() {
 	cat - 1>&2 <<EOF
-Usage: $0 <bundle> [ <file> ... ]"
+Usage: $0 <bundle> [ -r <name> ] [ <file> ... ]"
+
 Unzip a zipped bundle of LearningSuite submissions for an assigment, each with
 its own directory, named numerically (e.g., "001_...", "002_...", etc.).  Copy
-any files specified on the command line to each directory, e.g., for building,
-testing, grading, etc.
+the submission and any other files specified on the command line to the
+appropriate directory, e.g., for building, testing, grading, etc.  If the
+submitted file ends in tar, tar.gz, or tgz, then the file is also
+untarred/gzipped in the new directory.  If the -r option is used, then the copy
+of the file in the directory is renamed using the name specified.
 EOF
+}
+
+RENAME=
+while getopts "r:" opt; do
+	case $opt in
+		r)
+			RENAME=$OPTARG
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
+BUNDLE="$1"
+if [ "x$BUNDLE" = "x" ]; then
+	usage
 	exit 1
 fi
+shift
 
 unzip -q "$BUNDLE" || exit 1
 i=0
@@ -53,6 +74,9 @@ for f in *; do
 	$cmd "$f"
 	if (( $? != 0 )); then
 		echo -e "\033[33mError\033[0m: troubling extracting $f" 1>&2
+	fi
+	if [ "x$RENAME" != "x" ]; then
+		mv "$f" "$RENAME"
 	fi
 	cd ../
 	(( i++ ))
